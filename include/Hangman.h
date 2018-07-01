c7d7885637ab45b1e4c9c83bfbaeb5e7e87c7f8d
alexandermeek <alexandermeek09@gmail.com>
2018-07-01 14:08:23 +0100

    Lay out all required functions.

--- include/Hangman.h
+++ include/Hangman.h
@@ -1,16 +1,41 @@
 #ifndef HANGMAN_H
 #define HANGMAN_H
 
+#include <string>
+#include <vector>
 
 class Hangman
 {
     public:
-        Hangman();
+        Hangman(std::string word, int maxGuesses);
         virtual ~Hangman();
 
+        std::string getWord();
+        std::vector<char> getGuessedLetters();
+        int getIncorrectGuesses();
+        int getGuessesLeft();
+        int getTotalGuesses();
+        bool guess(char letter);
+        bool guess(std::string word);
+        bool isFinished();
+        bool isWon();
+        bool isLost();
     protected:
 
     private:
+        std::string word;
+        std::vector<char> guessedLetters;
+        int lengthOfWord;
+        int incorrectGuesses; //Number of incorrect guesses.
+        int maxGuesses; //Number of guesses allowed.
+        int totalGuesses; //Number of guesses the user has taken.
+        int totalWordGuessed; //Number of letters from the word that have been guessed.
+
+        void checkValidGuess(char letter);
+        int findOccurrencesOf(char letter);
+        char charToUpper(char character);
+        int getLengthOfWord();
+        std::string getFoundWord();
 };
 
 #endif // HANGMAN_H
--- src/Hangman.cpp
+++ src/Hangman.cpp
@@ -1,11 +1,96 @@
 #include "Hangman.h"
+#include <string>
+#include <vector>
 
-Hangman::Hangman()
+using namespace std;
+
+Hangman::Hangman(string word, int maxGuesses)
 {
-    //ctor
+    this->word = word;
+    this->maxGuesses = maxGuesses;
 }
 
 Hangman::~Hangman()
 {
     //dtor
+}
+
+string Hangman::getWord()
+{
+    return word;
+}
+
+vector<char> Hangman::getGuessedLetters()
+{
+    return guessedLetters;
+}
+
+int Hangman::getIncorrectGuesses()
+{
+    return 0;
+}
+
+int Hangman::getGuessesLeft()
+{
+    return 0;
+}
+
+int Hangman::getTotalGuesses()
+{
+    return 0;
+}
+
+bool Hangman::guess(char letter)
+{
+    return false;
+}
+
+bool Hangman::guess(string word)
+{
+    return false;
+}
+
+bool Hangman::isFinished()
+{
+    return false;
+}
+
+bool Hangman::isWon()
+{
+    return false;
+}
+
+bool Hangman::isLost()
+{
+    return false;
+}
+
+void Hangman::checkValidGuess(char letter)
+{
+
+}
+
+int Hangman::findOccurrencesOf(char letter)
+{
+    return 0;
+}
+
+char Hangman::charToUpper(char character)
+{
+    return 'A';
+}
+
+int Hangman::getLengthOfWord()
+{
+    return 0;
+}
+
+string Hangman::getFoundWord()
+{
+    return "Hello";
+}
+
+int main()
+{
+    return 0;
 }
