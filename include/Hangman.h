7acce3e41f4855eea0dc3275e512cbeaa5f994fe
alexandermeek <alexandermeek09@gmail.com>
2018-07-01 12:55:00 +0100

    Add empty Hangman class

---
+++ include/Hangman.h
@@ -0,0 +1,16 @@
+#ifndef HANGMAN_H
+#define HANGMAN_H
+
+
+class Hangman
+{
+    public:
+        Hangman();
+        virtual ~Hangman();
+
+    protected:
+
+    private:
+};
+
+#endif // HANGMAN_H
